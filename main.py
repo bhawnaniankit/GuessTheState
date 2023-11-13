8cfd645e041ade8a1a13efcdb1377f271dd4a195
bhawnaniankit <bhawnaniankit@gmail.com>
2023-11-14 01:08:34 +0530

    Handeled AttributeError

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ abhay.penup()
 user_score=0
 while user_score<50:
     user_guess=screen.textinput(title=f"Enter a {user_score+1}/50 state ",prompt="Guess a state")
-    if(user_guess.title() in state_list and user_guess not in gussed):
-        gussed.append(user_guess)
-        user_score+=1
-        gussed_state=states[states["state"]==user_guess.title()]
-        abhay.goto(int(gussed_state["x"].to_list()[0]),int(gussed_state["y"].to_list()[0]))
-        abhay.write(user_guess.title(),True,"center")
+    try:
+        if(user_guess.title() in state_list and user_guess not in gussed ):
+            gussed.append(user_guess)
+            user_score+=1
+            gussed_state=states[states["state"]==user_guess.title()]
+            abhay.goto(int(gussed_state["x"].to_list()[0]),int(gussed_state["y"].to_list()[0]))
+            abhay.write(user_guess.title(),True,"center")
+    except AttributeError:
+        break
 screen.exitonclick()
